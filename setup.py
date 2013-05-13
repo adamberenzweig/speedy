b34638a732d81117140845abba82e7af44045afa
Russell Power <power@cs.nyu.edu>
2013-05-13 16:12:24 -0400

    Update docs.

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ Server
         def foo(self, handle, arg1, arg2):
             handle.done(do_something(arg1, arg2))
 
-    import rpc.server
-    s = rpc.server.RPCServer('localhost', 9999, handler=MyHandler())
+    import speedy.server
+    s = speedy.server.RPCServer('localhost', 9999, handler=MyHandler())
     s.start()
 
 Client
@@ -39,8 +39,8 @@ Client
 
 ::
 
-    import rpc.client
-    c = rpc.client.RPCClient('localhost', 9999)
+    import speedy.client
+    c = speedy.client.RPCClient('localhost', 9999)
     future = c.foo('Some data', 'would go here')
     assert future.wait() == 'Expected result.'
 
